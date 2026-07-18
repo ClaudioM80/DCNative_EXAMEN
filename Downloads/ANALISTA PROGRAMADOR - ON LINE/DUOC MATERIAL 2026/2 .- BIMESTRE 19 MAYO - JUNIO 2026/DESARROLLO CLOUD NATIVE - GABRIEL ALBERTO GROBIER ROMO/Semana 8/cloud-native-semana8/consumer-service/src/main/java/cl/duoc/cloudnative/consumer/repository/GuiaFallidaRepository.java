95057c7b08c668c9d7797ff8b959fc1e0fade70e
ClaudioM80 <cl.moralest@duocuc.cl>
2026-07-18 03:37:57 -0400

    Delete Downloads/ANALISTA PROGRAMADOR - ON LINE/DUOC MATERIAL 2026/2 .- BIMESTRE 19 MAYO - JUNIO 2026/DESARROLLO CLOUD NATIVE - GABRIEL ALBERTO GROBIER ROMO/Semana 8/cloud-native-semana8 directory

--- Downloads/ANALISTA PROGRAMADOR - ON LINE/DUOC MATERIAL 2026/2 .- BIMESTRE 19 MAYO - JUNIO 2026/DESARROLLO CLOUD NATIVE - GABRIEL ALBERTO GROBIER ROMO/Semana 8/cloud-native-semana8/consumer-service/src/main/java/cl/duoc/cloudnative/consumer/repository/GuiaFallidaRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.cloudnative.consumer.repository;
-
-import cl.duoc.cloudnative.consumer.model.GuiaFallida;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GuiaFallidaRepository extends JpaRepository<GuiaFallida, Long> {
-}
